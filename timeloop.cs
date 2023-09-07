4656c78baa8b1c2eb5c8aeeb3e2443c8b6ee920b
Adam Jones <106862447+unixthug@users.noreply.github.com>
2023-09-07 16:10:27 -0500

    Create timeloop.cs

---
+++ timeloop.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for(int i = 1 ; i <= n ; i++) {
+            Console.WriteLine(i + " Abracadabra");
+        }
+    }
+}
