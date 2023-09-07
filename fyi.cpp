fc1f0ead26da00bfee84670b1e1f1b8fbd07a583
Adam Jones <106862447+unixthug@users.noreply.github.com>
2023-09-07 16:18:27 -0500

    Create fyi.cpp

---
+++ fyi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    if (a > b) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
