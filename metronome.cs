ba73fe468798e78c9ee086305e5b8cd46a556d4d
Adam Jones <106862447+unixthug@users.noreply.github.com>
2023-09-07 16:13:06 -0500

    Create metronome.cs

---
+++ metronome.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(num / 4.0);
+    }
+}
